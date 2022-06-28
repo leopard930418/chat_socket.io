2b0b3d6e88c1affd541187fb5377e8b7ada873b8
leopard930418 <leopard930418@gmail.com>
2022-06-29 04:13:18 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+//   res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+      io.emit('chat message', msg); 
+    });
+  });
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
